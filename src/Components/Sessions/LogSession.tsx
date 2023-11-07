8092783e6d458dd1bbf5ec263e32c35c80697260
abhimessi16 <piabhi18@gmail.com>
2023-11-07 21:50:30 +0530

    session input feature added

--- src/Components/Sessions/LogSession.tsx
+++ src/Components/Sessions/LogSession.tsx
@@ -1,15 +1,27 @@
+
+import { useState, useEffect } from "react"
+
 import { Button } from "@nextui-org/react"
 import { Input } from "@nextui-org/react"
 
 const LogSession = () => {
+
+    const [sessionId, setSessionId] = useState("")
+
+    const handleSessionInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSessionId(e.target.value)
+    }
+
     return (
-        <div className="min-w-[25vw] min-h-[50vh] flex flex-col items-center justify-center m-8 p-4 rounded-xl shadow-[2px_0px_5px_1px] shadow-consumer">
+        <div className="min-w-[25vw] min-h-[50vh] bg- flex flex-col items-center justify-center m-8 p-4 rounded-xl shadow-[2px_0px_5px_1px] shadow-consumer">
             <p className="text-center">
-                Have a Session Id?<br/>
-                Log into the session.
+                Have invite for a session?<br/>
+                Log into that session.
             </p><br/>
-            <Input variant="bordered" label="Session ID" labelPlacement="outside" 
-            className="p-2 max-w-[250px] border-consumer"></Input>
+            <Input label="Session ID" labelPlacement="outside" 
+            className={"m-2 rounded-xl max-w-[250px] border-consumer " + (sessionId.length !== 0 && "border-1")}
+            onChange={handleSessionInput}
+            ></Input>
             <Button variant="bordered" className="border-consumer font-medium">Log in</Button>
         </div>
     )
